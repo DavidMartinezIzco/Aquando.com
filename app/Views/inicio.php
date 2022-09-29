96d86c4ca44bc9d87b57009de7b81738b08f00ac
RauberGraves <dmartinez@dateando.tech>
2022-09-29 15:55:17 +0200

    banner pt3
    Solo ejecuta en dispositivos no android (experimento)

--- app/Views/inicio.php
+++ app/Views/inicio.php
@@ -375,18 +375,11 @@ if (isset($_GET['log'])) {
         if (!isset($_SESSION['nombre']) && $_SESSION['mensajeDesc'] == true) {
             // echo '<img src="../public/logo.png"';
             echo "<h1 id='txtDesconectado' style='width:30%;margin:25% 40%;color:grey; transition:1.5s;opacity:60%'>Desconectado</h1>";
-            echo '<div style="z-index:99;background-color: rgb(1, 168, 184);width:100%;color:whitesmoke;padding:1em 3em;position:fixed;bottom:0;left:0">
-            <i class="fas fa-times" style="font-size: 150%" id="btnAyudaCerrar" onclick="cerrarBanner(this)"></i>
-            <h2>Descarga la app gratuita de Aquando para Android</h2>
-            <hr>
-            <button style="background-color: yellowgreen;padding:15px;color:whitesmoke;border-radius: 9px;border:2px solid whitesmoke">
-            Aquando<i class="fab fa-android"></i></button>
-            </div>';
-        }
-        ?>
-        
+            echo '<script>var ua = navigator.userAgent;if (!(ua.match(/Android/i))) {document.getElementById("contenido").innerHTML +=<div style="z-index:99;background-color: rgb(1, 168, 184);width:100%;color:whitesmoke;padding:1em 3em;position:fixed;bottom:0;left:0"><i class="fas fa-times" style="font-size: 150%" id="btnAyudaCerrar" onclick="cerrarBanner(this)"></i><h2>Descarga la app gratuita de Aquando para Android</h2><hr><button style="background-color: yellowgreen;padding:15px;color:whitesmoke;border-radius: 9px;border:2px solid whitesmoke">Aquando<i class="fab fa-android"></i></button></div>;</script>;';
+        } ?>
     </div>
 </body>
+
 <script>
 if (screen.width < 600) {
     document.getElementsByClassName('fas fa-tools')[0].style.display = 'none';
@@ -398,10 +391,9 @@ function cerrarMenuEsp() {
     $("#menuIzq").trigger('widthChange');
 }
 
-function cerrarBanner(e){
+function cerrarBanner(e) {
     e.parentElement.style.display = 'none';
 }
-
 </script>
 
 </html>
